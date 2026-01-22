1ad6e569be41cb40c257a58baa874428251557aa
thsvkd <thsxogud1123@gmail.com>
2026-01-22 17:43:20 +0900

    feat: update docker build script to support optional push flag
    - Added support for an optional '--push' flag in the docker build script.
    - Updated usage message to reflect the new parameter.
    
    Signed-off-by: thsvkd <thsxogud1123@gmail.com>

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 TAG=$1
+PUSH=""
+
+if [ "$2" = "--push" ]; then
+    PUSH="--push"
+fi
 
 if [ -z "$TAG" ]; then
-    echo "Usage: $0 <tag>"
+    echo "Usage: $0 <tag> [--push]"
     exit 1
 fi
 
-docker buildx build --platform linux/amd64,linux/arm64 -t mysmax/python-matter-server:latest -t mysmax/python-matter-server:$TAG --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t mysmax/python-matter-server:latest -t mysmax/python-matter-server:$TAG $PUSH .
